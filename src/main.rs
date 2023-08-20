92368ebd6d3b55458ba39320c633d3b2ab8fd758
Salil Shankar <salil@hey.com>
2023-08-21 03:22:55 +0530

    better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use std::fs;
 use std::env;
-fn main() {
+fn main() -> Result<(), &'static str> {
     let args: Vec<String> = env::args().collect();
-    match args.len() {
-        1 => panic!("No file provided"),
-        _ => {}
+    if args.len() == 1 {    
+        return Err("File not provided");
     }
     let file_path = &args[1];
     let content = 
@@ -12,6 +11,7 @@ fn main() {
                 Ok(file) => file,
                 Err(error) => panic!("File open failed with error: {}", error)
             };
-
     println!("{}", content);
+
+    Ok(())
 }
