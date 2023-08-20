bfc4d8961da1bbac00038169223a515708c4c720
Salil Shankar <salil@hey.com>
2023-08-21 01:59:20 +0530

    implement file read

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
+use std::fs;
 use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let file_path = &args[1];
+    let content = 
+            match fs::read_to_string(file_path) {
+                Ok(file) => file,
+                Err(error) => panic!("File open failed with error: {}", error)
+            };
+
+    println!("{}", content);
 }
