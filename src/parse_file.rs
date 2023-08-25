dc64481df23f27809402f07e282fb4906b8fdb70
Salil Shankar <salil@hey.com>
2023-08-26 02:17:55 +0530

    Verbatim from Reddit: 'Switch to 2 spaces and use it as motivation to nest as little as possible :P'

--- src/parse_file.rs
+++ src/parse_file.rs
@@ -1,16 +1,19 @@
 use std::fs;
 
 pub fn file(file_path: Result<String, String>) -> String {
-  let file_or_error = 
+  let content = 
     match file_path {
-      Ok(path) => {
-        match fs::read_to_string(path) {
-          Ok(file) => file,
-          Err(error) => format!("Error: {error}")
-        }
-      },
+      Ok(path) => read_file(path),
       Err(error) => format!("Error: {error}")
     };
 
-  return file_or_error
+  return content
+}
+
+fn read_file(path: String) -> String {
+  return 
+    match fs::read_to_string(path) {
+      Ok(file) => file,
+      Err(error) => format!("Error: {error}")
+  }
 }
